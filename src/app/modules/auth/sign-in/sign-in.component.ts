4a614ce1391bb73aa4e333bf48ddd90327ed1c8a
info1090 <info@asha-tech.co.th>
2023-03-27 15:28:37 +0700

    login

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -13,7 +13,7 @@ import { AuthService } from 'app/core/auth/auth.service';
         /* language=SCSS */
         `
             .light{
-                background:#2A2E45 !important;
+                background: linear-gradient(110deg, #62e6a5 50%, #9ae7ba 50%, #9ae7ba 52%, #d1f5de 52%, #d1f5de 0);
             }
 
 
