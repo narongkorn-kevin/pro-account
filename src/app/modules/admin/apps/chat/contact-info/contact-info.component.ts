5be726eeebb24b0f33a4143cdd4474d0320ff5eb
kommy1122 <kommy525@gmail.com>
2023-06-04 14:29:46 +0700

    mk

--- src/app/modules/admin/apps/chat/contact-info/contact-info.component.ts
+++ src/app/modules/admin/apps/chat/contact-info/contact-info.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, NgZone, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, NgZone, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { Chat } from 'app/modules/admin/apps/chat/chat.types';
 import { ItemService } from 'app/modules/admin/g-admin/livesteam/livemag/item.service';
@@ -9,7 +9,6 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { SaleOrderService } from 'app/modules/admin/g-admin/sale-order/sale-order.service';
 import { ChatService } from '../chat.service';
 
-
 @Component({
     selector: 'chat-contact-info',
     templateUrl: './contact-info.component.html',
@@ -17,18 +16,71 @@ import { ChatService } from '../chat.service';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactInfoComponent implements OnInit {
+    @ViewChild('productDialog') productDialog!: TemplateRef<any>;
     Data: any;
     selectedBank: string;
     banks: string[] = ['Kasikorn Bank', 'Bank of America', 'HSBC', 'Citibank'];
     images: any[] = [];
     dataRow: any;
 
-    rawDataFilter: any[] = []
-    formData: FormGroup
+    rawDataFilter: any[] = [];
+    formData: FormGroup;
 
     newSelectProducts: any[] = [];
 
     searchOrderField: FormControl = new FormControl(null, Validators.required);
+
+    rerender: any;
+    item$: Observable<any>;
+
+    @Input() chat: Chat;
+    @Input() drawer: MatDrawer;
+    firstFormGroup = this._formBuilder.group({
+        firstCtrl: ['', Validators.required],
+    });
+    secondFormGroup = this._formBuilder.group({
+        secondCtrl: ['', Validators.required],
+    });
+
+    selectedImage: string | undefined;
+    showProductList = false;
+    productList: any[] = [];
+    products: any[] = [
+        { name: 'Product 1', price: 10 },
+        { name: 'Product 2', price: 20 },
+        { name: 'Product 3', price: 15 }
+    ];
+
+    /**
+     * Constructor
+     */
+    constructor(
+        private itemService: ItemService,
+        private _changeDetectorRef: ChangeDetectorRef,
+        private _matDialog: MatDialog,
+        private _formBuilder: FormBuilder,
+        private ngZone: NgZone,
+        private _Service: ChatService,
+    ) {
+        this.formData = this._formBuilder.group({
+            customerName: [null],
+            phone: [null],
+            address: [null],
+        });
+    }
+
+    ngOnInit(): void {
+        this.item$ = this.itemService.getItemPage().pipe(
+            map((resp: any) => {
+                return resp.data.data;
+            })
+        );
+
+        this._Service.getOrder().subscribe((resp: any) => {
+            this.dataRow = resp.data;
+            this.rawDataFilter = this.dataRow;
+        });
+    }
 
     handleFileInput(event: any): void {
         this.ngZone.run(() => {
@@ -51,96 +103,26 @@ export class ContactInfoComponent implements OnInit {
         });
     }
 
-    rerender: any;
-    item$: Observable<any>;
-
-    @Input() chat: Chat;
-    @Input() drawer: MatDrawer;
-    firstFormGroup = this._formBuilder.group({
-        firstCtrl: ['', Validators.required],
-    });
-    secondFormGroup = this._formBuilder.group({
-        secondCtrl: ['', Validators.required],
-    });
-    /**
-     * Constructor
-     */
-    constructor(
-        private itemService: ItemService,
-        private _changeDetectorRef: ChangeDetectorRef,
-        private _matDialog: MatDialog,
-        private _formBuilder: FormBuilder,
-        private ngZone: NgZone,
-        private _Service: ChatService,
-
-    )
-
-
-     {
-
-
-        this.formData = this._formBuilder.group({
-            customerName: [null],
-            phone: [null],
-            address: [null],
-        })
-    }
-
-    ngOnInit(): void {
-        this.item$ = this.itemService.getItemPage().pipe(
-            map((resp: any) => {
-                return resp.data.data
-            })
-        );
-
-        this._Service.getOrder().subscribe((resp: any) => {
-            this.dataRow = resp.data;
-            this.rawDataFilter = this.dataRow
-            // console.log('dataRow',this.dataRow)
-        })
-
-    }
-
-
-
-
-
-
     New2() {
         const dialogRef = this._matDialog.open(AddProductComponent, {
             width: '900px',
             height: '750px'
         });
         dialogRef.afterClosed().subscribe(item => {
-
             this.rerender();
             this._changeDetectorRef.markForCheck();
             this.formData.patchValue({
-              weight: item,
-
+                weight: item,
             });
 
-            console.log(this.formData.value.item[1].name)
-        dialogRef.afterClosed().subscribe(items => {
-
-            this.newSelectProducts = items;
-            // this.rerender();
-            this._changeDetectorRef.markForCheck();
-            // this.formData.patchValue({
-            //     weight: item,
-
-            // });
-
-            // console.log(this.formData.value.item[1].name)
+            console.log(this.formData.value.item[1].name);
         });
 
-
-
-
-
-
+        dialogRef.afterClosed().subscribe(items => {
+            this.newSelectProducts = items;
+            this._changeDetectorRef.markForCheck();
+        });
     }
-    )}
 
     searchOrder() {
         this.itemService.searchOrder(this.searchOrderField.value).subscribe({
@@ -149,14 +131,39 @@ export class ContactInfoComponent implements OnInit {
                     customerName: [resp.name],
                     phone: [resp.telephone],
                     address: [resp.address],
-                })
+                });
 
                 this._changeDetectorRef.markForCheck();
             },
             error: (err) => {
                 alert(JSON.parse(err));
             }
-        })
+        });
     }
 
+    onFileSelected(event: any) {
+        const file: File = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = (e: any) => {
+                this.selectedImage = e.target.result;
+            };
+        }
+    }
+
+    getTotalPrice(): number {
+        let totalPrice = 0;
+        for (const product of this.productList) {
+            totalPrice += product.price;
+        }
+        return totalPrice;
+    }
+
+    onProductSelected(product: any) {
+        if (!this.productList.includes(product)) {
+            this.productList.push(product);
+            this.showProductList = true;
+        }
+    }
 }
