2a4cd85e254dc49605a662d4b09408a4e90b8c49
teekubs1 <nattapat.chinwatprapa@gmail.com>
2023-09-06 10:16:12 +0700

    แก้สี

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -13,7 +13,7 @@ import { AuthService } from 'app/core/auth/auth.service';
         /* language=SCSS */
         `
             .light{
-                background: linear-gradient(110deg, #010f20 50%, #010f20 50%, #010f20 52%, #010f20 52%, #010f20 0);
+                background: linear-gradient(110deg, #37565c 1%, #37565c 1%, #37565c 1%, #37565c 1%, #37565c 0);
             }
 
 
